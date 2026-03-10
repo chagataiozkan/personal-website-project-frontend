6fe1acea4b22442b5051e8d3858a6b6e915c0f8a
Çağatay Özkan <chagataiozkan@gmail.com>
2026-03-10 11:45:38 +0300

    implement responsive mobile layout with media queries

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,35 @@
-import { mockData } from "../data/mockData";
-import { useContext } from "react";
-import { LanguageContext } from "../contexts/LanguageContext";
+  import { mockData } from "../data/mockData";
+  import { useContext } from "react";
+  import { LanguageContext } from "../contexts/LanguageContext";
 
-export default function Projects() {
-  const { lang } = useContext(LanguageContext);
+  export default function Projects() {
+    const { lang } = useContext(LanguageContext);
 
-  return (
-    <section className="projects" id="projects">
-      <div className="container">
-        <div className="divider-line"></div>
-        <h2>{mockData.header.projectText[lang]}</h2>
+    return (
+      <section className="projects" id="projects">
+        <div className="container">
+          <div className="divider-line"></div>
+          <h2>{mockData.header.projectText[lang]}</h2>
 
-        <div className="projects-parts">
-          {mockData.projects.map((project) => (
-            <div key={project.id} className="projects-row">
-              <img src={`/images/project_image_${project.id}.png`} />
-              <h3>{project.title[lang]}</h3>
-              <p>{project.text[lang]}</p>
-              <div className="projects-row-pills">
-                <p>react</p>
-                <p>redux</p>
-                <p>axios</p>
+          <div className="projects-parts">
+            {mockData.projects.map((project) => (
+              <div key={project.id} className="projects-row">
+                <img src={`/images/project_image_${project.id}.png`} />
+                <h3>{project.title[lang]}</h3>
+                <p>{project.text[lang]}</p>
+                <div className="projects-row-pills">
+                  <p>react</p>
+                  <p>redux</p>
+                  <p>axios</p>
+                </div>
+                <div className="projects-row-details">
+                  <a href="https://github.com">Github</a>
+                  <a href="https://github.com">{project.viewSite[lang]}</a>
+                </div>
               </div>
-              <div className="projects-row-details">
-                <a href="https://github.com">Github</a>
-                <a href="https://github.com">{project.viewSite[lang]}</a>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
-  );
-}
+      </section>
+    );
+  }
